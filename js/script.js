90062572186d607d1f89579baf4e8f754e389aeb
Pavel <shabunyapavel1996@gmail.com>
2021-07-03 17:57:34 +0300

    Practic_part2

--- js/script.js
+++ js/script.js
@@ -11,19 +11,29 @@ const personalMovieDB = {
     privat: false,
 }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('на сколько оцените его? ', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его? ', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('на сколько оцените его? ', '');
+          if(a != null &&  b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+          }else{
+              console.log('error');
+              i--;
+          }
+}
 
+if(personalMovieDB.count < 10){
+    console.log('Вы смотрели мало фильмо!');
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель !');
+}else if(personalMovieDB.count >= 30 ){
+    console.log('Вы киноман ! Это круто !');
+}else{
+    console.log('ERROR ! ERROR ! ERROR ! ERROR !');
+}
 console.log(personalMovieDB);
-      
-
-
-
 
 
 
